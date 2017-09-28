1d2856df54e288e73784f5d9ee8415e44b61a87e
Alberto Tiboni <a.tiboni@gmail.com>
2017-09-28 16:58:57 +0200

    cosmetics

--- 01-Login/src/Home/Home.js
+++ 01-Login/src/Home/Home.js
@@ -4,26 +4,31 @@ class Home extends Component {
   componentWillMount() {
     this.setState({ profile: {} });
 
-    const { userProfile, getProfile } = this.props.auth;
+    const { userProfile, getProfile, isAuthenticated } = this.props.auth;
 
-    if (!userProfile) {
-        getProfile((err, profile) => {
-          this.setState({ profile });
-        });
-    } else {
-        this.setState({ profile: userProfile });
+    if(isAuthenticated()) {
+        if (!userProfile) {
+            getProfile((err, profile) => {
+              this.setState({ profile });
+            });
+        } else {
+            this.setState({ profile: userProfile });
+        }
     }
   }
 
+  goToLogin() {
+      this.props.history.replace('login')
+  }
 
-  login() {
-    this.props.auth.login();
+  goToRegister() {
+      this.props.history.replace('register')
   }
 
   getUserName() {
       return this.state.profile.name;
   }
-  
+
   render() {
     const { isAuthenticated } = this.props.auth;
     return (
@@ -41,9 +46,16 @@ class Home extends Component {
                 You are not logged in! Please{' '}
                 <a
                   style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
+                  onClick={this.goToLogin.bind(this)}
                 >
                   Log In
+                </a>
+                {' '} or{' '}
+                <a
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.goToRegister.bind(this)}
+                >
+                  Register
                 </a>
                 {' '}to continue.
               </h4>
